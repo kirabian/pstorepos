eef1ff4829a98d132fb30b445042f382869317bd
kirabian <fabiansyahalghiffarireal@gmail.com>
2026-01-09 20:52:35 +0700

    169

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/js/bootstrap-auth.js' // File baru
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    server: {
-        hmr: {
-            host: 'localhost',
-        },
-    },
 });
